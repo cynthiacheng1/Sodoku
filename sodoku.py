ac169e412621e7f193635836f20e995d0aa7209a
Cynthia Cheng <cynthia.cheng@CynthiacBookPro.fios-router.home>
2022-05-07 14:06:34 -0400

    update

--- sodoku.py
+++ sodoku.py
@@ -13,48 +13,6 @@ def loadInputFile(filename):
 print(loadInputFile("Sample_Input.txt"))
 
 
-# def printBoard(board):
-#     for i in range(0, 9):
-#         for j in range(0, 9):
-#             print(board[i][j], end=" ")
-#         print()
-
-# def isPossible(board, row, col, val):
-#     for j in range(0, 9):
-#         if board[row][j] == val:
-#             return False
-
-#     for i in range(0, 9):
-#         if board[i][col] == val:
-#             return False
-
-#     startRow = (row // 3) * 3
-#     startCol = (col // 3) * 3
-#     for i in range(0, 3):
-#         for j in range(0, 3):
-#             if board[startRow+i][startCol+j] == val:
-#                 return False
-#     return True
-
-# def solve():
-#     for i in range(0, 9):
-#         for j in range(0, 9):
-#             if board[i][j] == 0:
-#                 for val in range(1, 10):
-#                     if isPossible(board, i, j, val):
-#                         board[i][j] = val
-#                         solve()
-
-#                         # Bad choice, make it blank and check again
-#                         board[i][j] = 0
-#                 return
-#     # We found a solution, print it            
-#     printBoard(board)
-# print("\n\n")
-# printBoard(board)
-# print("\n\n")
-# solve()
-
 import sys
 import numpy as np
 from functools import reduce
