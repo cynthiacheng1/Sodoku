163edef29b131c2efbf514afd91f7a5c7926fadf
matt-swartz <mattcswartz@gmail.com>
2022-05-17 12:21:05 -0400

    final submission

--- sodoku.py
+++ sodoku.py
@@ -1,121 +1,155 @@
 # Sodoku CS 4613
 # Cynthia Cheng (cc5469) and Matthew Swartz (mcs871)  
 
+# initial empty game board
 board = []
+# load in game board from input file and place into board array variable
 def loadInputFile(filename):
     # open input file 
     with open(filename) as file:
         lines = [line.rstrip() for line in file]
         for i in range(len(lines)):
             board.append([int(x) for x in lines[i].split()])
-    return board 
 
-print(loadInputFile("Sample_Input.txt"))
+# LOAD THE INPUT FILE HERE
+loadInputFile("Input1.txt")
 
-
-import sys
+# import proper libraries
 import numpy as np
 from functools import reduce
 
-
-soduku = np.asarray(board)
-
+# convert board array to numpy array
+sodukuBoard = np.asarray(board)
+# this will be our tuples of 3 numbers for a row of a subgrid
 slices = [slice(0,3), slice(3,6), slice(6,9)]
 s1,s2,s3 = slices
-allgrids=[(si,sj) for si in [s1,s2,s3] for sj in [s1,s2,s3]] # Makes 2d slices for grids
+allgrids=[(si,sj) for si in [s1,s2,s3] for sj in [s1,s2,s3]]
 
-def var2grid(var):
-    "Returns the grid slice (3x3) to which the variable's coordinates belong "
+# finds the 3x3 grid slice the var coordinates belong in 
+def varToGrid(var):
     row,col = var
     grid = ( slices[int(row/3)], slices[int(col/3)] )
     return grid
 
-FULLDOMAIN = np.array(range(1,10)) #All possible values (1-9)
+# value range of 1-9 stored in array
+FULLDOMAIN = np.array(range(1,10))
 
 
-# Constraints
-def unique_rows(soduku):
-    for row in soduku:
-        if not np.array_equal(np.unique(row),np.array(range(1,10))) :
+# CONSTRAINTS
+# check that the rows of the solution has no repeats, i.e. one 1, one 2, ... , one 9 per row
+def unique_rows(sodukuBoard):
+    for row in sodukuBoard:
+        # if the row does not equal the array 1-9 then there is a repeat in the row, return false
+        if not np.array_equal(np.unique(row), np.array(range(1,10))):
             return False
     return True
-def unique_columns(soduku):
-    for row in soduku.T: #transpose soduku to get columns
-        if not np.array_equal(np.unique(row),np.array(range(1,10))) :
+# check that the colums of the solution have no repeated 1-9 values
+def unique_columns(sodukuBoard):
+    #transpose soduku to get columns
+    for col in sodukuBoard.T:
+        # same as above, compare column to array [1, 2, 3, ... , 9] i.e. no duplicates
+        if not np.array_equal(np.unique(col),np.array(range(1,10))) :
             return False
     return True
-
-def unique_grids(soduku):
-    for grid in allgrids: 
-        if not np.array_equal(np.unique(soduku[grid]),np.array(range(1,10))) :
+# check that each grid, the 9 3x3 squares, of the board have no repeated values
+def unique_grids(sodukuBoard):
+    for grid in allgrids:
+        # check a 3x3 grid is equivalent to [1, 2, 3, ... , 9] i.e. no duplicates in the subgrid
+        if not np.array_equal(np.unique(sodukuBoard[grid]),np.array(range(1,10))) :
             return False
     return True
-
-def  isComplete(soduku):
-    if 0 in soduku:
+# check the board for any 0's
+def isComplete(sodukuBoard):
+    if 0 in sodukuBoard:
         return False
     else:
         return True
-
-
-def checkCorrect(soduku):
-    if unique_columns(soduku):
-        if unique_rows(soduku):
-            if unique_grids(soduku):
+# checks all our constraints to see if our board is a valid complete solution
+def checkCorrect(sodukuBoard):
+    if unique_columns(sodukuBoard):
+        if unique_rows(sodukuBoard):
+            if unique_grids(sodukuBoard):
                 return True
     return False
 
 
-# Search
-def getDomain(var, soduku):
-    "Gets the remaining legal values (available domain) for an unfilled box `var` in `soduku`"
+# BACKTRACKING FUNCTIONS
+# returns an array of the available values between 1-9 avaible for this square to keep it within the constraints above
+def getDomainValues(var, sodukuBoard):
     row,col = var
-    #ravail = np.setdiff1d(FULLDOMAIN, soduku[row,:])
-    #cavail = np.setdiff1d(FULLDOMAIN, soduku[:,col])
-    #gavail = np.setdiff1d(FULLDOMAIN, soduku[var2grid(var)])
-    #avail_d = reduce(np.intersect1d, (ravail,cavail,gavail))
-    used_d = reduce(np.union1d, (soduku[row,:], soduku[:,col], soduku[var2grid(var)]))
+    # this creates an array of numbers between 1-9 which already exist in this row, column, or subgrid 
+    used_d = reduce(np.union1d, (sodukuBoard[row,:], sodukuBoard[:,col], sodukuBoard[varToGrid(var)]))
+    # values left to pick from for this square to make sure it still satisfies thee constraint
     avail_d = np.setdiff1d(FULLDOMAIN, used_d)
-    #print(var, avail_d)
     return avail_d
 
-def selectMRVvar(vars, soduku):
-    """
-    Returns the unfilled box `var` with minimum remaining [legal] values (MRV) 
-    and the corresponding values (available domain)
-    """
-    #Could this be improved?
-    avail_domains = [getDomain(var,soduku) for var in vars]
+# Minimum Remaining Value Heuristic
+def minimumRemainingVal(vars, sodukuBoard):
+    # gets our ORDER-DOMAIN-VALUES for the remaining 0's left on our board
+    avail_domains = [getDomainValues(var,sodukuBoard) for var in vars]
+    # the number of values a remaning zero can be, i.e. the size of one of the domains in avail_domain
     avail_sizes = [len(avail_d) for avail_d in avail_domains]
+    # the index of the domain with the smallest size
     index = np.argmin(avail_sizes)
+    # return the tuple of the coordinates of the 0 being replaced, and its possible domains
     return vars[index], avail_domains[index]
 
-def BT(soduku):
-    "Backtracking search to solve soduku"
-    # If soduku is complete return it.
-    if isComplete(soduku):
-        return soduku
-    # Select the MRV variable to fill
-    vars = [tuple(e) for e in np.transpose(np.where(soduku==0))]
-    var, avail_d = selectMRVvar(vars, soduku)
-    # Fill in a value and solve further (recursively), 
-    # backtracking an assignment when stuck
+# our actual backtracking algorithm
+def backtrackingSearch(sodukuBoard):
+    # solution is complete return the board
+    if isComplete(sodukuBoard):
+        return sodukuBoard
+    # our tuples of the coordinates of the 0's left in the board
+    vars = [tuple(e) for e in np.transpose(np.where(sodukuBoard==0))]
+    # find our zero to replace based on the MRV heuristic
+    var, avail_d = minimumRemainingVal(vars, sodukuBoard)
+    # recursively solve and backtrack an assignment when stuck
     for value in avail_d:
-        soduku[var] = value
-        result = BT(soduku)
+        # set 0 = to new value
+        sodukuBoard[var] = value
+        # recusive call
+        result = backtrackingSearch(sodukuBoard)
+        # checks if a result of the value placement works
         if np.any(result):
             return result
+        # reset board to zero as placement did not work with that number
         else:
-            soduku[var] = 0
+            sodukuBoard[var] = 0
+    # fails to find value
     return False
 
 
-# Solve
-print("solved:\n", BT(soduku))
-print("correct:", checkCorrect(soduku))
+# Solve the board
+backtrackingSearch(sodukuBoard)
+checkCorrect(sodukuBoard)
 
-
-# Output
+# Write solution to output file
 with open('output.txt','w') as f:
-    output_str = np.array2string(soduku.ravel(), max_line_width=90, separator='').strip('[]') + ' Solved with BTS'
+    output_str = ""
+    # loop through solution array 
+    for i in range(9):
+        for j in range(9):
+            # add number with a space
+            output_str += (str(sodukuBoard[i][j]) + " ")
+        # new line for each row
+        output_str += "\n"
+    
     f.write(output_str)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
